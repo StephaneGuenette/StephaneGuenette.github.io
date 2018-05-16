20239cdaeff8aefe92b22db0da79fb90be6764bb
Stephane <35612128+StephaneGuenette@users.noreply.github.com>
2018-05-16 01:46:12 -0400

    typed

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const mainlogo = document.querySelector(".headlogo");
 const back = document.querySelector(".btnBack");
+const h1 = document.querySelector("h1");
+firstSpan = document.querySelector(".typed");
+
+let typeRan = false;
 //door events
 left.addEventListener("click", openDoors);
 right.addEventListener("click", openDoors);
 back.addEventListener("click", closeDoors);
 //mainlogo.addEventListener("click", closeDoors);
+h1.addEventListener("click", function() {
+  if (firstSpan.textContent === "AM") {
+    var typed = new Typed(".typed", {
+      strings: ["Surf", "Design", "Code", "Love", "AM"],
+      typeSpeed: 100,
+      loop: false,
+      backSpeed: 50
+    });
+  }
+});
 
 function closeDoors() {
   left.classList.remove("animleft");
@@ -18,4 +32,16 @@ function openDoors() {
   left.classList.add("animleft");
   right.classList.add("animright");
   mainlogo.classList.add("logoapear");
+
+  if (!typeRan) {
+    setTimeout(() => {
+      var typed = new Typed(".typed", {
+        strings: ["Surf", "Design", "Code", "Love", "AM"],
+        typeSpeed: 100,
+        loop: false,
+        backSpeed: 50
+      });
+      typeRan = true;
+    }, 1000);
+  }
 }
